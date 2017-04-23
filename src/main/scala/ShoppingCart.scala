cbe3ac6b25909588eedb3d3653ea2691933f13b2
Tenoch De La Cruz <tenoch_delacruz@xoono.net>
2017-04-23 16:46:30 +0200

    Added main function printing list of items and calculated total

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,7 +1,17 @@
+
+import model.{Apple, Orange}
 import services.CheckoutService
 
 object ShoppingCart extends CheckoutService {
   def main(args: Array[String]): Unit = {
-
+    val scannedItems = List(
+      Apple(),
+      Apple(),
+      Orange(),
+      Apple()
+    )
+    val itemsToPrint = scannedItems.foldLeft("")( (r,c) => s"$r ${c.toString}")
+    val total = calculatePrice(scannedItems)
+    println(s"[$itemsToPrint ] => $total")
   }
 }
