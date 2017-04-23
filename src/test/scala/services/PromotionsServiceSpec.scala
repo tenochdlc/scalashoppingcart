2593207c53744dcb12e349a6d8e58b35b5976c66
Tenoch De La Cruz <tenoch_delacruz@xoono.net>
2017-04-23 16:54:58 +0200

    First calculate discounts test

--- src/test/scala/services/PromotionsServiceSpec.scala
+++ src/test/scala/services/PromotionsServiceSpec.scala
@@ -3,5 +3,10 @@ package services
 import org.scalatest.{FlatSpec, Matchers}
 
 class PromotionsServiceSpec extends FlatSpec with Matchers {
+  val serviceToTest = new PromotionsService {}
 
+  "calculateDiscounts" should "return 0 when the list is empty" in {
+    serviceToTest.calculateDiscounts(List.empty)
+      .equals(BigDecimal(0)) shouldBe true
+  }
 }
