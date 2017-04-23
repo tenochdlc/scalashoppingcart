a06db731bd78e1afdc4612b3ec4966690335065a
Tenoch De La Cruz <tenoch_delacruz@xoono.net>
2017-04-23 17:02:39 +0200

    Two more tests for calculate discounts

--- src/main/scala/services/PromotionsService.scala
+++ src/main/scala/services/PromotionsService.scala
@@ -2,13 +2,24 @@ package services
 
 import model.{Apple, Item, Orange}
 
+
 trait PromotionsService {
   val apple = Apple()
   val orange = Orange()
 
   def calculateDiscounts(basket:List[Item]) : BigDecimal= {
-    basket.isEmpty match {
-      case true => BigDecimal(0)
+    val appleCount = basket.count {
+      case Apple() => true
+      case _ => false
     }
+    val orangeCount = basket.count {
+      case Orange() => true
+      case _ => false
+    }
+
+    val appleDiscount = (appleCount / 2) * apple.price
+    val orangeDiscount = (orangeCount / 3) * orange.price
+
+    appleDiscount + orangeDiscount
   }
 }
--- src/test/scala/services/PromotionsServiceSpec.scala
+++ src/test/scala/services/PromotionsServiceSpec.scala
@@ -1,5 +1,6 @@
 package services
 
+import model.{Apple, Orange}
 import org.scalatest.{FlatSpec, Matchers}
 
 class PromotionsServiceSpec extends FlatSpec with Matchers {
@@ -8,5 +9,17 @@ class PromotionsServiceSpec extends FlatSpec with Matchers {
   "calculateDiscounts" should "return 0 when the list is empty" in {
     serviceToTest.calculateDiscounts(List.empty)
       .equals(BigDecimal(0)) shouldBe true
+  }
+
+  it should "return no discount (0) for one apple" in {
+    serviceToTest.calculateDiscounts(List(Apple()))
+      .equals(BigDecimal(0)) shouldBe true
+  }
+
+  it should "return no discount for 2 oranges" in {
+    val orange = Orange()
+    serviceToTest.calculateDiscounts(
+      List(orange)
+    ).equals(BigDecimal(0)) shouldBe true
   }
 }
