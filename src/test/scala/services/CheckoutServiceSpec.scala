4aa79028143a517cf0b17e537d573a665d89d1fd
Tenoch De La Cruz <tenoch_delacruz@xoono.net>
2017-04-23 16:40:28 +0200

    Second test for calculateprice

--- src/test/scala/services/CheckoutServiceSpec.scala
+++ src/test/scala/services/CheckoutServiceSpec.scala
@@ -1,12 +1,27 @@
 package services
 
+import model.{Apple, Orange}
 import org.scalatest.{FlatSpec, Matchers}
 
-class CheckoutServiceSpec  extends FlatSpec with Matchers {
+
+class CheckoutServiceSpec extends FlatSpec with Matchers {
+
   val serviceToTest = new CheckoutService {}
 
   "calculatePrice" should "return 0 when there are no items" in {
     val total = serviceToTest.calculatePrice(List.empty)
     total.equals(BigDecimal(0)) shouldBe true
+  }
+
+  it should "return the total cost of the items in the list" in {
+    val a1 = Apple()
+    val a2 = Apple()
+    val a3 = Apple()
+    val o1 = Orange()
+    val o2 = Orange()
+    val o3 = Orange()
+    val total = (a1.price * 3) + (o1.price * 3)
+
+    serviceToTest.calculatePrice(List(a1,a2,a3, o1,o2,o3)).equals(total) shouldBe true
   }
 }
